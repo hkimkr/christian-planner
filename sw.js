9e3a85dcaa149d119961f822730b9a25ff1fdaf0
hkimkr <116676060+hkimkr@users.noreply.github.com>
2026-07-25 03:09:06 +0900

    Improve mobile prayer cards and reordering

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "grace-planner-pages-v7-7";
+const CACHE_NAME = "grace-planner-pages-v7-8";
 const BASE_URL = new URL("./", self.location.href);
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./planner.html?v=7-7",
+  "./planner.html?v=7-8",
   "./app.css",
-  "./app.js?v=7-7",
+  "./app.js?v=7-8",
   "./manifest.webmanifest?v=7-4",
   "./icon.png?v=7-4",
 ].map((path) => new URL(path, BASE_URL).href);
